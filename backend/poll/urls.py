4496797c73be610fbdbe374ead517b1e78aaa33d
Jeeru Harshith Reddy <143481508+reddy-j-harshith@users.noreply.github.com>
2024-10-23 18:56:04 +0530

    Update urls.py

--- backend/poll/urls.py
+++ backend/poll/urls.py
@@ -29,10 +29,10 @@ urlpatterns = [
     path('get-net-ratings/', get_net_ratings, name='get_net_ratings'),
 
     # Test
-    path('test/create', views.create_test, name='create_test'),
-    path('test/fetch-all', views.fetch_tests, name='fetch_tests'),
-    path('test/<int:test_id>/start', views.start_test, name='start_test'),
-    path('test/<int:test_id>/stop', views.stop_test, name='stop_test'),
+    path('test/create/', views.create_test, name='create_test'),
+    path('test/fetch-all/', views.fetch_tests, name='fetch_tests'),
+    path('test/<int:test_id>/start/', views.start_test, name='start_test'),
+    path('test/<int:test_id>/stop/', views.stop_test, name='stop_test'),
 
     # Monitoring
     path('test/<int:test_id>/monitor', views.monitor_test, name='monitor_test'),
