8e794771523f03e4a455ced9fdfa70eb1ee5beb4
vishal-2022A7PS0174H <144363866+vishal-2022A7PS0174H@users.noreply.github.com>
2024-11-02 14:46:41 +0530

    Add files via upload

---
+++ poll/urls.py
@@ -0,0 +1,44 @@
+from django.urls import path
+from .views import *
+
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    # Auth
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # User
+    path('register/', register_user, name='register_user'),
+
+    # Question
+    path('questions/', get_questions, name='get_questions'),
+    path('question/<int:question_id>/', fetch_question, name='fetch_question'),
+    path('add-question/', add_question, name='add_question'),
+    path('update-question/<int:question_id>/', update_question, name='update_question'),
+    path('delete-question/<int:question_id>/', delete_question, name='delete_question'),
+
+    # Comment
+    path('comments/<int:question_id>/', get_comments, name='get_comments'),
+    path('add-comment/<int:question_id>/', add_comment, name='add_comment'),
+    path('delete-comment/<int:comment_id>/', delete_comment, name='delete_comment'),
+
+    # Rating
+    path('get_user_ratings/<int:question_id>/<int:user_id>/', get_user_ratings, name='get_rating'),
+    path('add-rating/<int:question_id>/<int:user_id>/', rate_question, name='add_rating'),
+    path('get-net-ratings/', get_net_ratings, name='get_net_ratings'),
+
+    # Test
+    path('test/create/', create_test, name='create_test'),
+    path('test/fetch-all/', fetch_tests, name='fetch_tests'),
+    path('test/<int:test_id>/start/', start_test, name='start_test'),
+    path('test/<int:test_id>/stop/', stop_test, name='stop_test'),
+
+    # Monitoring
+    path('test/<int:test_id>/monitor/', monitor_test, name='monitor_test'),
+    path('test/<int:user_id>/<int:test_id>/stats/', student_stats, name='student_stats'),
+    path('test/<int:test_id>/time/', get_remaining_time, name='fetch_time'),
+
+    # BULK ADD
+     path('bulk-add-questions/', bulk_add_questions, name='bulk_add_questions'),
+]
